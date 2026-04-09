073b2bb18559dc017fd22dde39ae51898ee3f824
BoyuXiao <2092327901@qq.com>
2026-04-09 11:27:15 +0800

    [Profile] Align pytest suite with YAML/Pydantic profiling flow

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -1,34 +1,9 @@
 import pytest
-from unittest.mock import patch, MagicMock
 import argparse
-from galvatron.core.arguments import galvatron_training_args, galvatron_profile_args, galvatron_profile_hardware_args, galvatron_search_args
 
-@pytest.mark.utils
-def test_galvatron_training_args():
-    """Test galvatron_training_args function"""
-    parser = argparse.ArgumentParser()
-    parser = galvatron_training_args(parser, use_megatron=True)
-    
-    # Parse args with default values
-    args = parser.parse_args([])
-    
-    # Test default values
-    assert args.set_model_config_manually == 0
-    assert args.global_train_batch_size == 32
-    assert args.dropout_prob == 0.1
-    assert args.epochs == 10
-    assert args.pp_deg == 2
-    assert args.mixed_precision == "bf16"
-    assert args.pipeline_type == "gpipe"
-    assert args.local_rank == -1  # when use_megatron=True
-    
-    # Test with use_megatron=False
-    parser = argparse.ArgumentParser()
-    parser = galvatron_training_args(parser, use_megatron=False)
-    args = parser.parse_args([])
-    assert args.local_rank == 0  # when use_megatron=False
-    assert args.lr == 1e-4
-    assert args.gpu_id == 0
+from galvatron.core.profiler.arguments import galvatron_profile_args, galvatron_profile_hardware_args
+from galvatron.core.search_engine.arguments import galvatron_search_args
+
 
 @pytest.mark.utils
 def test_galvatron_profile_args():
@@ -53,6 +28,7 @@ def test_galvatron_profile_args():
     assert not args.use_flash_attn
     assert args.shape_order == "SBH"
 
+
 @pytest.mark.utils
 def test_galvatron_profile_hardware_args():
     """Test galvatron_profile_hardware_args function"""
@@ -69,11 +45,10 @@ def test_galvatron_profile_hardware_args():
     assert args.master_port == "$MASTER_PORT"
     assert args.node_rank == "$RANK"
     assert args.max_tp_size == 8
-    assert args.backend == "nccl"
-    assert args.start_mb == 16
-    assert args.end_mb == 512
-    assert args.scale == 2
-    assert args.avg_or_min_or_first == "first"
+    assert args.envs == []
+    assert args.max_pp_deg == 8
+    assert args.overlap_time_multiply == 4
+
 
 @pytest.mark.utils
 def test_galvatron_search_args():
@@ -102,13 +77,16 @@ def test_galvatron_search_args():
     assert args.costmodel_coe == 1.0
     assert args.fine_grained_mode == 1
 
+
 @pytest.mark.utils
-@pytest.mark.parametrize("args_func", [
-    galvatron_training_args,
-    galvatron_profile_args,
-    galvatron_profile_hardware_args,
-    galvatron_search_args
-])
+@pytest.mark.parametrize(
+    "args_func",
+    [
+        galvatron_profile_args,
+        galvatron_profile_hardware_args,
+        galvatron_search_args,
+    ],
+)
 def test_argument_groups(args_func):
     """Test if argument groups are correctly created"""
     parser = argparse.ArgumentParser()
@@ -116,12 +94,10 @@ def test_argument_groups(args_func):
     
     # Check if argument group exists
     group_titles = [group.title for group in parser._action_groups]
-    
+
     expected_titles = {
-        galvatron_training_args: "Galvatron Training Arguments",
         galvatron_profile_args: "Galvatron Profiling Arguments",
         galvatron_profile_hardware_args: "Galvatron Profiling Hardware Arguments",
-        galvatron_search_args: "Galvatron Searching Arguments"
+        galvatron_search_args: "Galvatron Searching Arguments",
     }
-    
     assert expected_titles[args_func] in group_titles
